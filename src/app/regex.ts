8114bb03de6157864e64c7e8199e5e98659070b9
GuichaouaT <guichaoua.tristan@gmail.com>
2020-04-14 16:27:34 +0200

    add firstname/lastname regex

--- src/app/regex.ts
+++ src/app/regex.ts
@@ -1,3 +1,4 @@
-export const USERNAME_PATTERN: RegExp = /^[\w\d]{3,20}$/;
-export const PASSWORD_PATTERN: RegExp = /^.{8,}$/;
-export const EMAIL_PATTERN: RegExp = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+export const USERNAME_PATTERN = /^[\w\d]{3,20}$/;
+export const PASSWORD_PATTERN = /^.{8,}$/;
+export const EMAIL_PATTERN = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+export const FIRST_LASTNAME_PATTERN = /^.{3,}$/;
