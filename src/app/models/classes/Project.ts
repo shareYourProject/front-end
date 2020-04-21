46abf7ff1ce16821191f3c3ebd022c3b8c393f45
Tristan Guichaoua <baanlohdev@gmail.com>
2020-04-21 17:06:51 +0200

    rn CollectionBase file

--- src/app/models/classes/Project.ts
+++ src/app/models/classes/Project.ts
@@ -1,5 +1,5 @@
 import { ApiService } from 'src/app/services/api.service';
-import { Collectionable } from 'src/app/models/CollectionServiceBase';
+import { Collectionable } from 'src/app/models/CollectionBase';
 import { ProjectData } from '../api/project';
 
 export class Project implements Collectionable<Project> {
