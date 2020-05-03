a3a146fdef57e55f9c65f0addb59739761b9c711
Tristan Guichaoua <baanlohdev@gmail.com>
2020-05-03 15:14:48 +0200

    base element of PostPage

--- src/app/pages/post-page/post-page.component.ts
+++ src/app/pages/post-page/post-page.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+interface DumbComment {
+  content: string;
+}
+
 
 @Component({
   selector: 'app-post-page',
@@ -7,9 +13,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostPageComponent implements OnInit {
 
-  constructor() { }
+  private _projectID: string;
+  private _postID: string;
+  private _comments: DumbComment[] = [];
+
+  constructor(private route: ActivatedRoute) { }
+
+  get projectID() { return this._projectID; }
+
+  get postID() { return this._postID; }
+
+  get content(): string { return 'DUMB CONTENT'; }
+
+  get comments(): DumbComment[] { return this._comments;  }
 
   ngOnInit(): void {
+    this._projectID = this.route.parent.snapshot.params['id'];
+    this._postID = this.route.snapshot.params['postId'];
+
+    // Todo: Get post data from api
+    this._comments = [ 
+      {content: 'I like this ^^'},
+      {content: 'wow ! Awesome !'},
+      {content: 'comment 3'},
+    ]
+  }
+
+  loadMore(): void {
+    this._comments.push({content: 'Another Comment'});
   }
 
 }
