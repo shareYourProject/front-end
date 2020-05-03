f9c3cb9d30369207471e9b038100c38ac711be7d
Tristan Guichaoua <baanlohdev@gmail.com>
2020-05-03 15:46:03 +0200

    skeleton done

--- src/app/pages/post-page/post-page.component.ts
+++ src/app/pages/post-page/post-page.component.ts
@@ -1,8 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
+interface DumbPost {
+  author: DumbUser;
+  content: string;
+  comments: DumbComment[];
+  liked: boolean;
+}
+
 interface DumbComment {
   content: string;
+  //author: DumbUser;
+}
+
+interface DumbUser {
+  id: number;
+  username: string;
+  profilePictureUrl: string;
 }
 
 
@@ -15,7 +29,8 @@ export class PostPageComponent implements OnInit {
 
   private _projectID: string;
   private _postID: string;
-  private _comments: DumbComment[] = [];
+
+  private _post: DumbPost
 
   constructor(private route: ActivatedRoute) { }
 
@@ -23,24 +38,35 @@ export class PostPageComponent implements OnInit {
 
   get postID() { return this._postID; }
 
-  get content(): string { return 'DUMB CONTENT'; }
-
-  get comments(): DumbComment[] { return this._comments;  }
+  get post(): DumbPost { return this._post; }
 
   ngOnInit(): void {
     this._projectID = this.route.parent.snapshot.params['id'];
     this._postID = this.route.snapshot.params['postId'];
 
     // Todo: Get post data from api
-    this._comments = [ 
-      {content: 'I like this ^^'},
-      {content: 'wow ! Awesome !'},
-      {content: 'comment 3'},
-    ]
+    this._post = {
+      content: 'I am the content',
+      author: {
+        id: 42,
+        username: 'Alice',
+        profilePictureUrl: 'https://www.journaldebrazza.com/wp-content/uploads/2018/04/IMAGE-DE-PHOTO-DE-PROFIL-VIDE.png',
+      },
+      comments: [
+        { content: 'I like this ^^' },
+        { content: 'wow ! Awesome !' },
+        { content: 'comment 3' },
+      ],
+      liked: false,
+    };
   }
 
   loadMore(): void {
-    this._comments.push({content: 'Another Comment'});
+    this._post.comments.push({ content: 'Another Comment' });
+  }
+
+  onLikeClick(): void {
+    this._post.liked = !this._post.liked;
   }
 
 }
