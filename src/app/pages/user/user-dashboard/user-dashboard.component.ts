893b629feb15a89ad512c95d7a63021421ceefbd
Tristan Guichaoua <baanlohdev@gmail.com>
2020-05-11 17:37:44 +0200

    link with api

--- src/app/pages/user/user-dashboard/user-dashboard.component.ts
+++ src/app/pages/user/user-dashboard/user-dashboard.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { UserAccount } from 'src/app/models/classes/UserAccount';
+import { ApiService } from 'src/app/services/api.service';
 
 @Component({
   selector: 'app-user-dashboard',
@@ -7,9 +10,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserDashboardComponent implements OnInit {
 
-  constructor() { }
+  user: UserAccount;
 
-  ngOnInit(): void {
+  constructor(
+    private readonly route: ActivatedRoute,
+    private readonly api: ApiService,
+  ) {
+
+  }
+
+  async ngOnInit() {
+    /*
+    if (!this.api.user) throw new Error("Loggin needed.");
+    this.user = this.api.user;
+    */
+    // simulate user
+    this.user = new UserAccount(this.api, {
+      id: 0,
+      username: "AliceDu29",
+      firstname: "Alice",
+      lastname: "DuBois",
+      biography: "I like coding !",
+      email: "Alice.DuBois@notamail.com",
+      links: [],
+      project_ids: [],
+      skills: [],
+    });
   }
 
 }
