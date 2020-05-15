9bdf828d2cdb9d2553d220e0e4370aa1e0fd6aa0
Tristan Guichaoua <baanlohdev@gmail.com>
2020-05-15 17:41:45 +0200

    prepare html structure

--- src/app/pages/project/project-dashboard/project-dashboard.component.ts
+++ src/app/pages/project/project-dashboard/project-dashboard.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Project } from 'src/app/models/classes/Project';
 import { ApiService } from 'src/app/services/api.service';
+import { UserAccount } from 'src/app/models/classes/UserAccount';
 
 @Component({
   selector: 'app-project-dashboard',
@@ -11,6 +12,7 @@ import { ApiService } from 'src/app/services/api.service';
 export class ProjectDashboardComponent implements OnInit {
 
   project$: Promise<Project>;
+  members$: Promise<UserAccount[]>;
 
   constructor(
     private readonly route: ActivatedRoute,
@@ -20,6 +22,7 @@ export class ProjectDashboardComponent implements OnInit {
   ngOnInit(): void {
     const projectID = parseInt(this.route.parent?.snapshot?.params?.id);
     this.project$ = this.api.projects.get(projectID);
+    this.members$ = this.project$.then(p => p.getMembers());
   }
 
 }
