59f5ec07c846e7d2c7e102ed9d07e8316c7f9d2f
Tristan Guichaoua <baanlohdev@gmail.com>
2020-05-18 16:50:59 +0200

    add post form

--- src/app/pages/project/project-dashboard/project-dashboard.component.ts
+++ src/app/pages/project/project-dashboard/project-dashboard.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Project } from 'src/app/models/classes/Project';
 import { ApiService } from 'src/app/services/api.service';
 import { UserAccount } from 'src/app/models/classes/UserAccount';
 import { Post } from 'src/app/models/classes/Post';
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-project-dashboard',
@@ -12,19 +13,41 @@ import { Post } from 'src/app/models/classes/Post';
 })
 export class ProjectDashboardComponent implements OnInit {
 
+  postForm: FormGroup;
+  postError = false;
+
   project$: Promise<Project>;
   members$: Promise<UserAccount[]>;
   posts$: Promise<Post[]>;
 
+  private _project?: Project;
+
   constructor(
     private readonly route: ActivatedRoute,
-    private readonly api: ApiService
-  ) { }
+    private readonly api: ApiService,
+    private readonly formBuilder: FormBuilder,
+    private readonly router: Router,
+  ) {
+    this.postForm = formBuilder.group({
+      postContent: ['', Validators.minLength(5)]
+    })
+  }
 
   ngOnInit(): void {
     const projectID = parseInt(this.route.parent?.snapshot?.params?.id);
     this.project$ = this.api.projects.get(projectID);
+    this.project$.then(p => this._project = p);
     this.members$ = this.project$.then(p => p.getMembers());
+  }
+
+  async onSubmitPost() {
+    if (this._project && this.postForm.valid) {
+      const post = await this._project.posts.create(this.postForm.value.postContent).catch(() => { });
+      if (post)
+        this.router.navigateByUrl(`/project/${this._project.id}/post/${post.id}`);
+      else
+        this.postError = true;
+    }
   }
 
 }
