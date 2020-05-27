b676469d290033dfcab5daaea69e44201fc17b4d
Tristan Guichaoua <33934311+baanloh@users.noreply.github.com>
2020-05-27 20:20:58 +0200

    Project settings page minimal (#120)
    * merge dev
    
    * 🚧 project settings page minimal

--- src/app/pages/project/project-settings/project-settings.component.ts
+++ src/app/pages/project/project-settings/project-settings.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Project } from 'src/app/models/classes/Project';
+import { IProject } from 'src/app/models/objectInterfaces/IProject';
 
 @Component({
   selector: 'app-project-settings',
@@ -10,6 +11,10 @@ import { Project } from 'src/app/models/classes/Project';
 export class ProjectSettingsComponent implements OnInit {
 
   project: Project;
+  data: IProject;
+
+  busy = false;
+  result: "none" | "success" | "error" = "none";
 
   constructor(private route: ActivatedRoute) { }
 
@@ -17,8 +22,16 @@ export class ProjectSettingsComponent implements OnInit {
     this.route.parent?.data.subscribe(
       (data: { project: Project }) => {
         this.project = data.project;
+        this.data = this.project.getEditableData();
       }
     )
   }
 
+  async submit() {
+    console.log('submit', this.data);
+    this.busy = true;
+    this.data.links = undefined as any; // TODO : https://github.com/shareYourProject/back-end/issues/62
+    this.result = (await this.project.update(this.data)) ? "success" : "error";
+    this.busy = false;
+  }
 }
