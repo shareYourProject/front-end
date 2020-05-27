4a79208c4fdc80ef0bbd9961d74c9308d28a9cda
Tristan Guichaoua <33934311+baanloh@users.noreply.github.com>
2020-05-27 19:44:58 +0200

    User settings page minimal (#118)
    * 🚧 minimal user settings + ts logic
    
    * minimal user setting page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
-import { ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -57,10 +57,11 @@ import { UserCardComponent } from './components/user-card/user-card.component';
     UserCardComponent
   ],
   imports: [
+    FormsModule,
+    ReactiveFormsModule,
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    ReactiveFormsModule,
     CommonModule,
   ],
   providers: [
--- src/app/pages/user/user-settings/user-settings.component.ts
+++ src/app/pages/user/user-settings/user-settings.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { User } from 'src/app/models/classes/User';
+import { IUserAccount } from 'src/app/models/objectInterfaces/IUserAccount';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-user-settings',
@@ -10,15 +12,35 @@ import { User } from 'src/app/models/classes/User';
 export class UserSettingsComponent implements OnInit {
 
   user: User;
+  data: IUserAccount;
 
-  constructor(private readonly route: ActivatedRoute) { }
+  firstName: string;
+
+  form: FormGroup;
+
+  busy = false;
+  result: "none" | "success" | "error" = "none";
+
+  constructor(
+    private readonly route: ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
     this.route.parent?.data.subscribe(
       (data: { user: User }) => {
         this.user = data.user;
+        this.data = this.user.getEditableData();
       }
     );
   }
+
+  async submit() {
+    console.log('submit', this.data);
+    this.busy = true;
+    this.result = (await this.user.update(this.data)) ? "success" : "error";
+    this.busy = false;
+  }
+
+
 
 }
