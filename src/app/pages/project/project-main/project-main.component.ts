ff97a795d50c0d3aff9de26006501f748a7b8787
Tristan Guichaoua <33934311+baanloh@users.noreply.github.com>
2020-05-28 23:58:09 +0200

    Add navbar to project dashboard to navigate throught sub pages (#128)
    * 🚧 add side bar
    
    * 💄 sub navbar skeleton
    
    * 💄 make main navbar over all
    
    * 💄 side navbar margin
    
    * :lipstick: Update UI sub-navbar Style
    
    * Updating branche (#139)
    
    * 🚧 Post Card component
    
    * :lipstick: Adding post card component UI
    
    * 🚑 fix project dashboard page (#130)
    
    * :lipstick: Updating post UI
    
    * 👽 Project add owner id (#132)
    
    * Post page add comment (#133)
    
    * ✨ comment form
    
    * 🚑 fix comment create
    
    * Post page add comment (#134)
    
    * ✨ comment form
    
    * 🚑 fix comment create
    
    * ✨ reset comment form after submit if success
    
    * :lipstick: Update UI style
    
    * ✨ getLikedPosts
    
    * ✨ user public page load liked post
    
    * :poop: Adding post list loading in front
    
    * 🔊  add error log on PostService
    
    * :lipstick: Update Project Public Page UI
    
    * :heavy_plus_sign: Adding AwesomeFont
    
    Co-authored-by: Tristan Guichaoua <baanlohdev@gmail.com>
    Co-authored-by: Tristan Guichaoua <33934311+baanloh@users.noreply.github.com>
    
    * :pencil2: Adding new font
    
    * :lipstick: Adding icons
    
    * :lipstick: Change sub-navbar UI style
    
    Co-authored-by: Killian Mahé <killianmahe.pro@gmail.com>
    Co-authored-by: Killian Mahé <53428491+killian-mahe@users.noreply.github.com>

--- src/app/pages/project/project-main/project-main.component.ts
+++ src/app/pages/project/project-main/project-main.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Project } from 'src/app/models/classes/Project';
 import { ActivatedRoute } from '@angular/router';
+import { faTachometerAlt, faHome, faSlidersH, IconDefinition } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-project-main',
@@ -8,7 +10,26 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class ProjectMainComponent implements OnInit {
 
-  constructor() { }
+  project: Project;
+  url: string;
 
-  ngOnInit(): void { }
+  readonly navs: { name: string, url: string , icon: IconDefinition}[] = [
+    { name: 'Dashboard', url: 'dashboard', icon: faHome },
+    { name: 'Settings', url: 'settings', icon: faSlidersH }
+  ]
+
+  constructor(private readonly route: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    this.route.data.subscribe(
+      (data: { project: Project }) => {
+        console.log(data);
+        this.project = data.project;
+        this.url = `/project/${this.project.id}/`;
+      }
+    )
+  }
+
+  // Icons
+  faTachometerAlt = faTachometerAlt;
 }
