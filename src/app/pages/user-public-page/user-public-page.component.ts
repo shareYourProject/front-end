a8e8408ba413609e62c1027474519352245ecf61
Tristan Guichaoua <baanlohdev@gmail.com>
2020-05-28 17:06:29 +0200

    ✨ user public page load liked post

--- src/app/pages/user-public-page/user-public-page.component.ts
+++ src/app/pages/user-public-page/user-public-page.component.ts
@@ -3,6 +3,8 @@ import { ActivatedRoute } from '@angular/router';
 import { User } from 'src/app/models/classes/User';
 import { Project } from 'src/app/models/classes/Project';
 import { ProjectService } from 'src/app/services/project.service';
+import { Post } from 'src/app/models/classes/Post';
+import { PostService } from 'src/app/services/post.service';
 
 @Component({
   selector: 'app-user-public-page',
@@ -13,11 +15,13 @@ export class UserPublicPageComponent implements OnInit {
 
   user: User;
   projects$: Promise<Project[]>;
+  likedPosts$: Promise<Post[]>;
   tab: Tabs = "tab 1";
 
   constructor(
     private readonly route: ActivatedRoute,
-    private readonly projects: ProjectService
+    private readonly projects: ProjectService,
+    private readonly posts: PostService,
   ) { }
 
   ngOnInit(): void {
@@ -25,6 +29,7 @@ export class UserPublicPageComponent implements OnInit {
       (data: { user: User }) => {
         this.user = data.user;
         this.projects$ = this.projects.getMany(this.user.projectIds);
+        this.likedPosts$ = this.posts.getLikedPosts(this.user.id);
       }
     )
   }
