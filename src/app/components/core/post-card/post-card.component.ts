3310ea971aed1273deaaaea9ae66fa01337dd817
Tristan Guichaoua <33934311+baanloh@users.noreply.github.com>
2020-05-31 18:58:10 +0200

    Post card buttons (#171)
    * ⚡️ impl post card button
    
    * ⚡️ update post card in project/user public page

--- src/app/components/core/post-card/post-card.component.ts
+++ src/app/components/core/post-card/post-card.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Post } from 'src/app/models/classes/Post';
+import { ActivatedRoute } from '@angular/router';
+import { User } from 'src/app/models/classes/User';
 
 @Component({
   selector: 'app-post-card',
@@ -9,10 +11,28 @@ import { Post } from 'src/app/models/classes/Post';
 export class PostCardComponent implements OnInit {
 
   @Input() post: Post;
+  @Input() me?: User | null;
 
-  constructor() { }
+  constructor(private readonly route: ActivatedRoute) { }
 
   ngOnInit(): void {
+  }
+
+  isLiked() {
+    return !!this.me && this.post.likerIds.includes(this.me.id);
+  }
+
+  async likeButtonClick(btn: HTMLButtonElement) {
+    if (!this.me) return;
+    btn.disabled = true;
+    try {
+      if (this.isLiked()) {
+        await this.post.unlike();
+      } else {
+        await this.post.like();
+      }
+    } catch (e) { console.error('fail like button', e); }
+    btn.disabled = false;
   }
 
 }
