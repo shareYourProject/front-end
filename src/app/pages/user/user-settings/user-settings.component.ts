e7098a4684ff53b178188d35a16ad5c276580ff5
Killian Mahé <53428491+killian-mahe@users.noreply.github.com>
2020-06-01 11:26:45 +0200

    User settings page (#189)
    * Names and Biography in form with html / css
    
    * Update branch (#188)
    
    * Fix #161 (#163)
    
    * 🚑 add post, get method that need no return value
    
    * 🐛 fix Project.addMember
    
    * ✨ add getFollowedProject (#168)
    
    * :lipstick: Improve user list UI (#170)
    
    * Follow buttons (#169)
    
    * 🚧 project impl follow button
    
    * 🚧 public user follow button
    
    * 👽 add User followed project ids
    
    * ⚡️ impl follow button project public page
    
    * 🔥 remove follow button user public page
    
    * 🚑 fix project follow button
    
    * 🐛 fetch user after follow
    
    * ⚡️ 💄 disable follow button while api request
    
    * Post card buttons (#171)
    
    * ⚡️ impl post card button
    
    * ⚡️ update post card in project/user public page
    
    * 💄Update dashbord members UI (#172)
    
    * Project sub navbar links (#173)
    
    * ⚡️ navbar link are permissions aware
    
    * ⚡️ Project getPermissions return full for owner
    
    * 🐛 fix import
    
    * ⚡️ dont show owner in member page (#176)
    
    * 👽 🚑 fix post create route (#179)
    
    * :lipstick: Update UI Main Dashboard (#180)
    
    * 🐛 fix cors from post create request (#181)
    
    * 💄 Update UI for research feature (#184)
    
    * 💄 Add name on user card (#185)
    
    Co-authored-by: Tristan Guichaoua <33934311+baanloh@users.noreply.github.com>
    
    * 💄Update UI
    
    * ⚡️ Change form to dynamic
    
    * 🎨 remove useless import/var
    
    * 💄 Fix UI
    
    Co-authored-by: Arcadeuh <fouque.bastien@outlook.fr>
    Co-authored-by: Tristan Guichaoua <33934311+baanloh@users.noreply.github.com>
    Co-authored-by: Tristan Guichaoua <baanlohdev@gmail.com>

--- src/app/pages/user/user-settings/user-settings.component.ts
+++ src/app/pages/user/user-settings/user-settings.component.ts
@@ -1,8 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { User } from 'src/app/models/classes/User';
-import { IUser } from 'src/app/models/object interfaces/IUser';
-import { FormGroup } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, FormArray } from '@angular/forms';
+import { faPlus, faSave, faTrashAlt } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-user-settings',
@@ -10,37 +10,90 @@ import { FormGroup } from '@angular/forms';
   styleUrls: ['./user-settings.component.scss']
 })
 export class UserSettingsComponent implements OnInit {
+  // Icons
+  faPlus = faPlus;
+  faSave = faSave;
+  faTrashAlt = faTrashAlt;
 
   user: User;
-  data: IUser;
-
-  firstName: string;
-
   form: FormGroup;
-
-  busy = false;
   result: "none" | "success" | "error" = "none";
 
   constructor(
-    private readonly route: ActivatedRoute
-  ) { }
+    private readonly route: ActivatedRoute,
+    private readonly formBuilder: FormBuilder,
+  ) {
+    this.form = formBuilder.group({
+      username: ['', Validators.required],
+      email: ['', Validators.required],
+      firstname: ['', Validators.required],
+      lastname: ['', Validators.required],
+      skills: formBuilder.array([]),
+      biography: '',
+      links: formBuilder.array([])
+    })
+  }
+
+  get linksControl() { return (this.form.get('links') as FormArray); }
+
+  get skillsControl() { return (this.form.get('skills') as FormArray); }
+
 
   ngOnInit(): void {
     this.route.parent?.data.subscribe(
       (data: { user: User }) => {
         this.user = data.user;
-        this.data = this.user.getEditableData();
+        this.patchForm();
       }
     );
   }
 
-  async submit() {
-    console.log('submit', this.data);
-    this.busy = true;
-    this.result = (await this.user.update(this.data)) ? "success" : "error";
-    this.busy = false;
+  private patchForm() {
+    const data = this.user.getEditableData();
+    this.form.patchValue(data);
+
+    const skillsControl = this.skillsControl;
+    for (const skills of data.skills)
+      skillsControl.push(this.createSkill(skills));
+
+    const linksControl = this.linksControl;
+    for (const link of data.links)
+      linksControl.push(this.createLink(link.key, link.value));
   }
 
+  createSkill(value?: string) {
+    return this.formBuilder.control(value ?? '');
+  }
 
+  createLink(key?: string, value?: string) {
+    return this.formBuilder.group({
+      key: key ?? '',
+      value: value ?? '',
+    });
+  }
 
+  addLink() {
+    (this.form.get('links') as FormArray).push(this.createLink());
+  }
+
+  removeLink(i: number) {
+    (this.form.get('links') as FormArray).removeAt(i);
+  }
+
+  addSkill() {
+    (this.form.get('skills') as FormArray).push(this.createSkill());
+  }
+
+  removeSkill(i: number) {
+    (this.form.get('skills') as FormArray).removeAt(i);
+  }
+
+  async submit() {
+    if (this.form.valid) {
+      this.form.disable();
+      console.log('submit', this.form.value);
+      this.result = (await this.user.update(this.form.value)) ? "success" : "error";
+      this.form.enable();
+    }
+  }
 }
