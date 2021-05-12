5a02e0a15b068eaa9987ed46e744d90cc7f00533
Killian Mahé <killianmahe.pro@gmail.com>
2021-05-12 23:53:45 +0200

    Add comment in posts
    Signed-off-by: Killian Mahé <killianmahe.pro@gmail.com>

--- src/components/cards/CommentCard.vue
+++ src/components/cards/CommentCard.vue
@@ -6,13 +6,16 @@
         <div class="comment">
             <!-- Author name -->
             <div class="mb-1">
-                <span class="leading-4 text-sm font-medium block">{{comment.author.full_name}}  • <span class="text-xs font-light">{{timeSinceCreation}}</span></span>
+                <span
+                    class="leading-4 text-xs font-medium block">{{ comment.author.first_name }} {{ comment.author.last_name }}  • <span
+                    class="text-xs font-light">{{ timeSinceCreation }}</span></span>
             </div>
 
             <!-- Comment content -->
-            <div ref="content" class="text-sm content overflow-hidden" v-html="comment.formated_content" :class="{'max-h-4': !expand}">
+          <div ref="content" class=" content overflow-hidden" v-html="comment.formated_content"
+               :class="{'max-h-4': !expand}">
 
-            </div>
+          </div>
             <div v-if="overflow" class="absolute right-2 bottom-1 cursor-pointer hover:underline text-sm text-onyx-700" @click="on_expand">
                 ...read more
             </div>
@@ -22,41 +25,47 @@
 
 
 <script lang="ts">
-import { defineComponent, PropType } from 'vue'
+import {defineComponent, PropType} from 'vue'
 import moment from 'moment';
-import { Comment } from '../../models'
+import {Comment} from '../../models'
 
 export default defineComponent({
-    // props: {
-    //     comment: {
-    //         type: Object as PropType<Comment>,
-    //         required: true
-    //     }
-    // },
-    // data() {
-    //     return {
-    //         overflow: {
-    //             type: Boolean,
-    //             default: false
-    //         },
-    //         expand: false
-    //     }
-    // },
-    // computed: {
-    //     timeSinceCreation(): string {
-    //         return moment(this.comment.created_at).fromNow();
-    //     }
-    // },
-    // mounted() {
-    //     let el = this.$refs.content as HTMLDivElement
-    //     this.overflow = el.clientWidth < el.scrollWidth || el.clientHeight < el.scrollHeight;
-    // },
-    // methods: {
-    //     on_expand() {
-    //         this.overflow = false;
-    //         this.expand = true;
-    //     }
-    // }
+  props: {
+    comment: {
+      type: Object as PropType<Comment>,
+      required: true
+    }
+  },
+  data() {
+    return {
+      overflow: false,
+      expand: false
+    }
+  },
+  computed: {
+    timeSinceCreation(): string {
+      return moment(this.comment.created_at).fromNow();
+    }
+  },
+  mounted() {
+    let el = this.$refs.content as HTMLDivElement
+    this.overflow = el.clientWidth < el.scrollWidth || el.clientHeight < el.scrollHeight;
+  },
+  methods: {
+    on_expand() {
+      this.overflow = false;
+      this.expand = true;
+    }
+  }
 })
 </script>
 
+<style scoped>
+.expand-btn {
+  @apply cursor-pointer ml-2 text-base leading-6 font-semibold text-onyx-500;
+}
+
+.comment {
+  @apply rounded-b-xl rounded-tr-xl ml-3 bg-gray-200 w-full px-3 py-2 relative;
+}
+</style>
