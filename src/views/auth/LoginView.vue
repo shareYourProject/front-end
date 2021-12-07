e29fb7cbd061a1382fd649e44fb7acae541853e4
Killian Mahé <killianmahe.pro@gmail.com>
2021-12-06 22:27:20 -0500

    🐛 Login error messages
    Signed-off-by: Killian Mahé <killianmahe.pro@gmail.com>

--- src/views/auth/LoginView.vue
+++ src/views/auth/LoginView.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="flex w-full h-screen">
-        <img class="hidden md:block w-auto h-full bg-blur-3" src="@/assets/img/default/jeff-sheldon-8z2Q6XWLYa4.jpg" alt="" />
+        <img class="hidden md:block w-auto h-full" src="@/assets/img/default/jeff-sheldon-8z2Q6XWLYa4.jpg" alt=""/>
         <div class="absolute right-0 px-12 md:px-24 pt-16 h-full py-12 bg-white w-full md:w-1/2 lg:w-5/12 justify-center">
             <h1 class="text-onyx-600 font-sans font-bold text-center text-4xl mb-6">Login</h1>
             <hr class="mb-12 mx-3">
@@ -36,22 +36,27 @@ import CustomInput from '@/components/inputs/CustomInput.vue'
 import {mapActions, mapGetters} from 'vuex'
 import {API} from '@/api'
 
+interface IError {
+  email: string,
+  password: string
+}
+
 export default defineComponent({
-    components: {
-        CustomInput
-    },
-    data() {
-        return {
-            form: {
-                email: "",
-                password: ""
-            },
-            errors: {
-                email: "",
-                password: ""
-            },
-            credentialError: ""
-        }
+  components: {
+    CustomInput
+  },
+  data() {
+    return {
+      form: {
+        email: "",
+        password: ""
+      },
+      errors: {
+        email: "",
+        password: ""
+      } as IError,
+      credentialError: ""
+    }
     },
     computed: {
         ...mapGetters({
@@ -72,8 +77,6 @@ export default defineComponent({
         },
         async onSubmit() {
 
-            console.log("On Submit");
-
             await API.csrf();
 
             API.login(this.form).then(response => {
@@ -91,18 +94,12 @@ export default defineComponent({
 
                         break;
 
-                    case 422:
-                        // Invalid form
-                        // let errorsObject = response.data.errors;
-
-                        // for (var key in errorsObject) {
-                        //     if (errorsObject.hasOwnProperty(key)) {
-                        //         errorsObject[key] = errorsObject[key][0];
-                        //     }
-                        // }
-
-                        // this.errors = errorsObject;
-                        break;
+                  case 422:
+                    for (let errorsKey in this.errors) {
+                      this.errors[errorsKey as keyof IError] = response.data.errors[errorsKey] ? response.data.errors[errorsKey][0] : ""
+                    }
+                    this.credentialError = response.data.message;
+                    break;
 
                     case 401:
                         this.credentialError = response.data;
--- src/views/auth/RegisterView.vue
+++ src/views/auth/RegisterView.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="flex w-full h-screen">
-        <img class="hidden md:block w-auto h-full bg-blur-3" src="@/assets/img/default/jeff-sheldon-8z2Q6XWLYa4.jpg" alt="" />
+        <img class="hidden md:block w-auto h-full" src="@/assets/img/default/jeff-sheldon-8z2Q6XWLYa4.jpg" alt=""/>
         <div class="absolute right-0 px-12 pt-16 md:h-full py-12 bg-white w-full md:w-1/2 lg:w-5/12 justify-center">
             <h1 class="text-onyx-600 font-sans font-bold text-center text-4xl mb-6">Register</h1>
             <hr class="mb-12 mx-3">
@@ -52,28 +52,37 @@ import {API} from '@/api'
 import {mapActions, mapGetters} from 'vuex'
 import CustomInput from '@/components/inputs/CustomInput.vue'
 
+interface IError {
+  first_name: string,
+  last_name: string,
+  username: string,
+  email: string,
+  password: string,
+  password_confirmation: string
+}
+
 export default defineComponent({
-    components: {
-        CustomInput
-    },
-    data() {
-        return {
-            form: {
-                first_name: "",
-                last_name: "",
+  components: {
+    CustomInput
+  },
+  data() {
+    return {
+      form: {
+        first_name: "",
+        last_name: "",
                 username: "",
                 email: "",
                 password: "",
                 password_confirmation:""
             },
             errors: {
-                first_name: "",
-                last_name: "",
-                username: "",
-                email: "",
-                password: "",
-                password_confirmation:""
-            }
+              first_name: "",
+              last_name: "",
+              username: "",
+              email: "",
+              password: "",
+              password_confirmation: ""
+            } as IError
         }
     },
     computed: {
@@ -86,14 +95,14 @@ export default defineComponent({
             me: 'me'
         }),
         resetErrors() {
-            this.errors = {
-                first_name: "",
-                last_name: "",
-                username: "",
-                email: "",
-                password: "",
-                password_confirmation: ""
-            };
+          this.errors = {
+            first_name: "",
+            last_name: "",
+            username: "",
+            email: "",
+            password: "",
+            password_confirmation: ""
+          } as IError;
         },
         async onSubmit(): Promise<void> {
           await API.csrf()
@@ -113,13 +122,9 @@ export default defineComponent({
                 break;
 
               case 422:
-
-                // for (const key in response.data.errors) {
-                //     if (Object.prototype.hasOwnProperty.call(response.data.errors, key)) {
-                //         this.errors[key] = response.data.errors[key][0];
-                //     }
-                // }
-
+                for (let errorsKey in this.errors) {
+                  this.errors[errorsKey as keyof IError] = response.data.errors[errorsKey] ? response.data.errors[errorsKey][0] : ""
+                }
                 break;
 
               default:
